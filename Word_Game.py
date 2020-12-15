eac78b56b49161ffa8a2c1c5b847ed4a2f2947ee
Tyler Solley <tjsolley1@gmail.com>
2020-12-14 22:11:07 -0800

    first commit

---
+++ Word_Game.py
@@ -0,0 +1,11 @@
+"""
+This a program designed for enjoyment. It is a game where the player has ten tries to guess a word from a
+certian list specified.  
+This program will be structed by gathering user name
+Setting a timer for user to begin guessing
+Importing the randomly selcted words list
+setting the number of tries the user has
+use a while loop to count the number of tries
+print results to user (Win or Lose)
+"""
+
